0fef8baa96e3300c7a58b000d45507a7b08146f8
javierortegon <javierortegonmp@gmail.com>
2019-05-16 10:51:49 -0500

    AR with 3d Button animation

---
+++ Assets/btn_animate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class btn_animate : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("LacieBtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        cubeAni.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("cube_animation");
+        Debug.Log("BTN Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("BTN Released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
