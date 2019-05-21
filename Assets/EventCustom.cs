1c67d1eda68eb67df2207e908d0e3a60817f4955
javierortegon <javierortegonmp@gmail.com>
2019-05-21 16:46:04 -0500

    ajustes finales

---
+++ Assets/EventCustom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class EventCustom : DefaultTrackableEventHandler
+{
+    public Animator cubeAni;
+
+    protected override void OnTrackingFound()
+    {
+        //cubeAni.Play("cube_animation");
+        //Debug.Log("Entro posicion: x : " + transform.position.x + " y: " + +transform.position.y + " z: " + +transform.position.z);
+        Debug.Log("entro");
+        base.OnTrackingFound();
+
+    }
+
+    protected override void OnTrackingLost()
+    {
+       
+        Debug.Log("salio");
+        base.OnTrackingLost();
+    }
+}
+
--- Assets/btn_animate.cs
+++ Assets/btn_animate.cs
@@ -6,31 +6,46 @@ using Vuforia;
 public class btn_animate : MonoBehaviour, IVirtualButtonEventHandler
 {
     public GameObject vbBtnObj;
-    public Animator cubeAni;
+    public Animator modelAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
-        vbBtnObj = GameObject.Find("LacieBtn");
-        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
-        cubeAni.GetComponent<Animator>();
+        
+        modelAnimation.GetComponent<Animator>();
+        //model.GetComponent<Animator>();
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
-        cubeAni.Play("cube_animation");
+        //cubeAni.Play("cube_animation");
         Debug.Log("BTN Pressed");
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
-        cubeAni.Play("none");
+        //cubeAni.Play("none");
         Debug.Log("BTN Released");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //modelAnimation.Play("Jog In Circle");
+        //model.Play("cube_animation");
+        //cubeAni.Play("cube_animation");
+        Debug.Log("Update Entro posicion: x : " + transform.position.x + " y: " + +transform.position.y + " z: " + +transform.position.z);
+        float limit = -0.03F;
+        if (transform.position.y < limit)
+        {
+            modelAnimation.Play("Jog In Circle");
+            Debug.Log("Abajo");
+        }
+        else
+        {
+            modelAnimation.Play("None");
+            //modelAnimation.Play("mixamo.com");
+            //    //    cubeAni.Play("none");
+        }
     }
 }
